d9bbe432345616b10fabee53834b39f5abbb4008
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-04-22 11:53:00 +0700

    tests

--- test/dart_either_test.dart
+++ test/dart_either_test.dart
@@ -1,1 +1,12 @@
+import 'package:test/test.dart';
+import 'package:dart_either/dart_either.dart';
 
+void main() {
+  group('Either', () {
+    group('constructors', () {
+      test('Either.left', () {
+        expect(Either.left(1), isA<Left<int>>());
+      });
+    });
+  });
+}
